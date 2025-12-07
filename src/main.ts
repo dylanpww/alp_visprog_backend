52c7d5beeec2da6543b0a9f40e56944c9a6694cb
ClarrenceAdriano <cadriano@student.ciputra.ac.id>
2025-12-07 22:00:56 +0700

    main.ts

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import express from "express"
+import { PORT } from "./utils/env-util"
+import { publicRouter } from "./routes/public-api"
+import { privateRouter } from "./routes/private-api"
+
+const app = express()
+
+app.use(express.json())
+app.use("/api", publicRouter)
+app.use("/api", privateRouter)
+app.listen(PORT || 3000, () => {
+    console.log('Conected to port ${PORT}')
+})

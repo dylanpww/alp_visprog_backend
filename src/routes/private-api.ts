d21efc16f5ad856177cbd42bd3623c3e85c48788
Vanness Aurelius Gunawan <vaurelius03@student.ciputra.ac.id>
2025-12-08 20:43:39 +0700

    routes_category

--- src/routes/private-api.ts
+++ src/routes/private-api.ts
@@ -1,6 +1,14 @@
 import express from "express"
 import { authMiddleware } from "../middlewares/auth-middleware"
+import { CategoryController } from "../controllers/category-controller"
 
 export const privateRouter = express.Router()
 
 privateRouter.use(authMiddleware)
+
+// Category Routes
+privateRouter.get("/categories", CategoryController.getAllCategories);
+privateRouter.get("/categories/:categoryId", CategoryController.getCategory);
+privateRouter.post("/categories", CategoryController.createCategory);
+privateRouter.put("/categories/:categoryId", CategoryController.updateCategory);
+privateRouter.delete("/categories/:categoryId", CategoryController.deleteCategory);

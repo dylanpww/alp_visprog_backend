0e2ae1c0602843b85a30b374d20d4068115760b6
dylanpww <dylanpatrickw@gmail.com>
2025-12-12 01:52:34 +0700

    plan-controller

---
+++ src/controllers/plan-controller.ts
@@ -0,0 +1,113 @@
+import { NextFunction, Request, Response } from 'express'
+import { PlanService } from '../services/plan-service'
+import { 
+    CreatePlanRequest, 
+    UpdatePlanRequest, 
+    AddDestinationRequest, 
+    UpdateDestinationStatusRequest,
+    toPlanResponse
+} from '../models/plan-model'
+
+export class PlanController {
+
+    static async getAllPlans(req: Request, res: Response, next: NextFunction) {
+        try {
+            const userId = Number(req.query.userId) 
+            
+            const response = await PlanService.getAllPlans(userId)
+            
+            res.status(200).json({
+                data: response 
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async getPlan(req: Request, res: Response, next: NextFunction) {
+        try {
+            const planId = Number(req.params.planId)
+            const planRaw = await PlanService.getPlan(planId)
+            const response = toPlanResponse(planRaw)
+            res.status(200).json({
+                data: response
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async createPlan(req: Request, res: Response, next: NextFunction) {
+        try {
+            const request = req.body as CreatePlanRequest
+            const response = await PlanService.createPlan(request)
+            res.status(201).json({
+                data: response
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async updatePlan(req: Request, res: Response, next: NextFunction) {
+        try {
+            const planId = Number(req.params.planId)
+            const request = req.body as UpdatePlanRequest
+            
+            const response = await PlanService.updatePlan(planId, request)
+            res.status(200).json({
+                data: response
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async deletePlan(req: Request, res: Response, next: NextFunction) {
+        try {
+            const planId = Number(req.params.planId)
+            await PlanService.deletePlan(planId)
+            res.status(200).json({
+                data: "Plan deleted successfully"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async addDestination(req: Request, res: Response, next: NextFunction) {
+        try {
+            const planId = Number(req.params.planId)
+            const { destinationId } = req.body as AddDestinationRequest
+            
+            const response = await PlanService.addDestination(planId, destinationId)
+            res.status(201).json({
+                data: response,
+                message: "Successfully added to planner"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async removeDestination(req: Request, res: Response, next: NextFunction) {
+        try {
+            const destinationPlanId = Number(req.params.id)
+            await PlanService.removeDestination(destinationPlanId)
+            res.status(200).json({
+                data: "Item removed from plan"
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+    static async updateDestinationStatus(req: Request, res: Response, next: NextFunction) {
+        try {
+            const destinationPlanId = Number(req.params.id)
+            const { isVisited } = req.body as UpdateDestinationStatusRequest
+            
+            const response = await PlanService.updateStatus(destinationPlanId, isVisited)
+            res.status(200).json({
+                data: response
+            })
+        } catch (error) {
+            next(error)
+        }
+    }
+}

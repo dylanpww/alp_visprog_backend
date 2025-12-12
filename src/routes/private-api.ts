93ac1859aac21ebd1b89c52d99023a816a9136a4
dylanpww <dylanpatrickw@gmail.com>
2025-12-12 11:59:32 +0700

    plan-routes

--- src/routes/private-api.ts
+++ src/routes/private-api.ts
@@ -1,6 +1,16 @@
 import express from "express"
 import { authMiddleware } from "../middlewares/auth-middleware"
+import { PlanController } from "../controllers/plan-controller"
 
 export const privateRouter = express.Router()
 
 privateRouter.use(authMiddleware)
+
+privateRouter.get('/plans', PlanController.getAllPlans)
+privateRouter.get('/plans/:planId', PlanController.getPlan)
+privateRouter.post('/plans', PlanController.createPlan)
+privateRouter.patch('/plans/:planId', PlanController.updatePlan)
+privateRouter.delete('/plans/:planId', PlanController.deletePlan)
+privateRouter.post('/plans/:planId/destinations', PlanController.addDestination)
+privateRouter.delete('/plans/items/:id', PlanController.removeDestination)
+privateRouter.patch('/plans/items/:id/status', PlanController.updateDestinationStatus)
